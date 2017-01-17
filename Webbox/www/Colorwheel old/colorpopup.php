97421d06743e58d797f1546cffa36d3a41636ba2
Fonzo <aggadur@gmail.com>
2017-01-17 08:54:55 +0100

    Colorwheel

---
+++ Webbox/www/Colorwheel old/colorpopup.php
@@ -0,0 +1,31 @@
+<?
+$HTML = '<!DOCTYPE html>
+<html>
+	<head>
+	<title>Color Popup</title>
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+	<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+	<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+</head>
+<body>
+ 
+<div data-role="page">
+ 
+	<!-- <div data-role="header">
+		<h1>Meine mobile Website</h1>
+	</div> -->
+ 
+	<div data-role="content">
+		<p>Hier der normale Seiteninhalt..</p>
+		<div class = "colorbox">Hallo</div>
+	</div>
+ 
+	<!-- <div data-role="footer">
+		<p>Allgemeine Seiteninfo</p>
+	</div> -->
+</div>
+ 
+</body>
+</html>';
+echo $HTML;
+?>
